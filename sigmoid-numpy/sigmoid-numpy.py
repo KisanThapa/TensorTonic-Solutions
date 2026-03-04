39295b5866dbe3935976f6e4b422d0f0377b4653
Kisan Thapa <kisanthapa33@gmail.com>
2026-03-04 11:33:45 -0500

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,4 +4,8 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    return 1 / (1 + np.exp(-1 * np.array(x)))
+    # 1. Convert given python list into numpy array
+    x = np.asarray(x, dtype=np.float32)
+
+    # 2. Calculate sigmoid = 1 / (1 + e^-x) 
+    return 1 / (1 + np.exp(-x))
